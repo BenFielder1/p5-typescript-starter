930a6015d46e393d200296a3f02a88cb8e3786a8
Joe Sharp <ratracejoe@hotmail.com>
2020-10-06 21:54:53 +0100

    working...kinda

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useSketch from './useSketch';
+
+import sketches from './sketches';
+
 
 function App() {
+  // Point the useSketch at a different sketch
+  const { refContainer } = useSketch(sketches.someSketch);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ref={refContainer} />
   );
 }
 
---
+++ src/sketches/index.ts
@@ -0,0 +1,7 @@
+import gravityOrbits from "./gravityOrbits";
+import someSketch from "./someSketch";
+
+export default {
+  gravityOrbits,
+  someSketch
+};
